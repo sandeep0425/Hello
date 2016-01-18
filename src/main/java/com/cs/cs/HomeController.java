00732d2937f9eb998ce8be83221b1f87c25f7ee3
sandeep <sandeepreddy.425@gmail.com>
2016-01-18 16:49:38 -0500

    second commti

--- src/main/java/com/cs/cs/HomeController.java
+++ src/main/java/com/cs/cs/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@Controller //controller stereotype
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
